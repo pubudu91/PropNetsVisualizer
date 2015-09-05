f6512fd94e1efd050b11830e8d91403d0568cfe5
Pubudu Fernando <pubudu.12@cse.mrt.ac.lk>
2015-09-06 04:58:22 +0530

    gulpfile edited

--- gulpfile.js
+++ gulpfile.js
@@ -243,7 +243,7 @@ gulp.task('default', ['clean'], function (cb) {
     // Note: add , 'precache' , after 'vulcanize', if your are going to use Service Worker
 });
 
-//gulp.task('heroku:production', ['default']);
+gulp.tasks('heroku:prod', ['default']);
 // Load tasks for web-component-tester
 // Adds tasks for `gulp test:local` and `gulp test:remote`
 require('web-component-tester').gulp.init(gulp);
